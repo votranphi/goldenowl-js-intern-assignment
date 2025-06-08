833aa9538b5de45fdd8c3f3f6e900c6f6b32fb6d
Vo Tran Phi <songokupkj@gmail.com>
2025-06-08 11:45:30 -0700

    refactor(develop): change the api documentation endpoint

--- g-scores-api/src/main.ts
+++ g-scores-api/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   const port = configService.get<number>('SERVER_PORT') || 3000;
   await app.listen(port, '0.0.0.0', () => {
