1220d3b2afabf020287282ddb75788cb8b39bf9f
Vo Tran Phi <songokupkj@gmail.com>
2025-06-08 15:42:27 +0700

    fix(backend): wrong frontend port

--- g-scores-api/src/main.ts
+++ g-scores-api/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
